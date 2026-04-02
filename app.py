30d1a49a6cce40e2a12611d8f0de6fda36a060b8
philipjob-png <philip.job@ndsu.edu>
2026-04-02 11:07:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(page_title="Stock Data Extraction App", layout="wide")
 
 # Main title of the app
-st.page_title("Stock Data Extraction App")
+st.title("Stock Data Extraction App")
 
 # Short Description of the Subtitle
 st.write("Extract stock market data from Yahoo Finance using a ticker symbol.")
