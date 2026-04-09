fd1d8ff66de61659bc7fef1a6998042acf4b2333
philipjob-png <philip.job@ndsu.edu>
2026-04-09 10:20:19 -0500

    Update bmiapp.py

--- bmiapp.py
+++ bmiapp.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("BMI Calculator")
-st.write("Calculate BMI"):
+st.write("Calculate BMI")
 #Input fields
 weight = st.number_input("Enter your weight (kg)", min_value=1.0, max_value=500.0, value=70.0, step=0.1)
 height = st.number_input("Enter your height (meters)" min_value=0.5, max_value=3.0, value=1.75, step=0.01)
